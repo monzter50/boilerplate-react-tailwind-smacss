f35cc528f72dfca58f5ba2496cf013dd3dbbf796
monster <josemonzter50@gmail.com>
2019-11-05 21:39:22 -0500

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Navbar = () =>{
+    return(
+        <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+  <div className="flex items-center flex-shrink-0 text-white mr-6">
+    <span className="font-semibold text-xl tracking-tight">Tailwind CSS</span>
+  </div>
+ 
+  <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+    <div className="text-sm lg:flex-grow">
+      <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+        Docs
+      </a>
+      <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+        Examples
+      </a>
+      <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+        Blog
+      </a>
+    </div>
+    <div>
+      <a href="#" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
+    </div>
+  </div>
+</nav>
+    )
+}
+export default Navbar
